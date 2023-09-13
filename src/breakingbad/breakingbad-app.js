d413e51cb6960d05b32878dce4bfd6afa263b24a
Daniel Gómez H <danielivgho5@gmail.com>
2023-09-13 18:46:06 -0500

    First commit

---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,41 @@
+/**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async () => {
+  const res = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
+
+  const data = await res.json();
+
+  console.log(data[0]);
+  return data;
+};
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const BreakingbadApp = async (element) => {
+  document.querySelector("#app-title").innerHTML = "Breaking Bad App";
+  element.innerHTML = "Loading...";
+
+  await fetchQuote();
+
+  const quoteLabel = document.createElement("blockquote");
+  const authorLabel = document.createElement("h3");
+  const nextQuoteButton = document.createElement("button");
+  nextQuoteButton.innerHTML = "Next Quote";
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.slice(0)[0].quote;
+    authorLabel.innerHTML = data.slice(0)[0].author;
+
+    element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+  };
+
+  nextQuoteButton.addEventListener("click", async () => {
+    element.innerHTML = "Loading...";
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+  fetchQuote().then(renderQuote);
+};
