7d93fd59aaa9b324e907e04ee8e9e6225d7da37f
Daniel Gómez H <danielivgho5@gmail.com>
2023-09-18 19:50:47 -0500

    funcionalidad render modal agregada

---
+++ src/users/uses-cases/save-user.js
@@ -0,0 +1,36 @@
+import { User } from "../models/user";
+
+/**
+ *
+ * @param {Like<User>} userLike
+ */
+export const saveUser = async (userLike) => {
+  const user = new User(userLike);
+  //TODO: FALTA UN MAPPER
+
+  if (user.id) {
+    throw "No implementada la actualización";
+  }
+
+  const updatedUser = await createUser(user);
+  return updatedUser;
+};
+
+/**
+ * @param {Like<User>} user
+ */
+const createUser = async (user) => {
+  const url = `${import.meta.env.VITE_BASE_URL}/users`;
+  const res = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const newUser = await res.json();
+  console.log({ newUser });
+
+  return newUser;
+};
